88b966f515e405943d604c5348f4ade250bb2682
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2025-04-10 11:45:02 +0200

    broadcating message

---
+++ src/main/java/org/example/ClientHandler.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+import java.security.PrivateKey;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket; // socket from server class
+    private BufferedReader bufferedReader; // read messages from server
+    private BufferedWriter bufferedWriter; // send aessages to client
+    private String clientUserName; // for user names
+
+    // ClientHandler constructor
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUserName = bufferedReader.readLine();
+            clientHandlers.add(this);
+            broadcastMessage("Server: " + clientUserName + " has entered tyhe chat!");
+        } catch (IOException e) {
+            closeEveryThing(socket, bufferedWriter, bufferedReader);
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+
+    public void broadcastMessage(String messageToSend) {
+        for (ClientHandler clientHandler : clientHandlers) {
+            try {
+                if (!clientHandler.clientUserName.equals(clientUserName)) {
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+ }
